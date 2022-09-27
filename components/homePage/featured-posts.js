820f5c4000d977280bf1cbd0456b296f306f99ed
Daria Piersa <dariapiersa17@gmail.com>
2022-09-27 13:03:12 +0200

    rendering dummy data post data

--- components/homePage/featured-posts.js
+++ components/homePage/featured-posts.js
@@ -1,10 +1,11 @@
+import PostGrid from '../posts/posts-grid';
 import classes from './featured-posts.module.css'
 
-function FeaturedPost() {
+function FeaturedPost(props) {
 
     return<section className={classes.latest}>
         <h2>Featured Posts</h2>
-         A list of posts
+         <PostGrid posts={props.posts}/>
     </section>
 }
 
--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import classes from './post-item.module.css'
 
@@ -10,13 +11,14 @@ function PostItem(props) {
         year:'numeric'
     });
 
-    const imagePath =`/images/posts/${slug}/${image}`
+    const imagePath =`/images/posts/${slug}/${image}`;
+    const linkPath = '/posts/${slug}'
 
     return <li className={classes.post}>
-        <Link>
+        <Link href={linkPath}>
         <a>
             <div className={classes.image}>
-                <Image src={imagePath} alt={title} width={300} height={200}/>
+                <Image src={imagePath} alt={title} width={300} height={200} layout="responsive"/>
             </div>
             <div classes={classes.content}>
                 <h3>{title}</h3>
