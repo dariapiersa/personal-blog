e0b5247301bf001317b4efd8fff4e8bb545c15d8
Daria Piersa <dariapiersa17@gmail.com>
2022-10-07 13:10:04 +0200

    storing message with mongoDb in a database

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,29 +1,71 @@
+import { useState } from "react";
 import classes from "./contact-form.module.css";
 
-function ContactForm(){
+function ContactForm() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredMessage, setEnteredMessage] = useState("");
 
-    return <section className={classes.contact}>
-        <h1>How can I help you?</h1>
-        <form className={classes.form}>
-            <div className={classes.controls}>
-                <div className={classes.control}>
-                    <label htmlFor="email">Your email</label>
-                    <input type="email" id="email" required />
-                </div>
-                <div className={classes.control}>
-                    <label htmlFor="name">Your Name</label>
-                    <input type="text" id="name" required />
-                </div>
-            </div>
-            <div className={classes.constrol}>
-                <label htmlFor="message">Your message</label>
-                <textarea id="message" rows="5"></textarea>
-            </div>
-            <div className={classes.actions}>
-            <button>Send message</button>
-            </div>
-        </form>
+  function sendMessageHandler(event) {
+    event.preventDefault();
+
+    //optional: add client-side validation
+
+    fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify({ //field name is in API route function // api/contact.js
+        email: enteredEmail,
+        name: enteredName,
+        message: enteredMessage
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+
+  return (
+    <section className={classes.contact}>
+      <h1>How can I help you?</h1>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
+        <div className={classes.controls}>
+          <div className={classes.control}>
+            <label htmlFor="email">Your email</label>
+            <input
+              type="email"
+              id="email"
+              required
+              value={enteredEmail}
+              onChange={(event) => setEnteredEmail(event.target.value)}
+            />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="name">Your Name</label>
+            <input
+              type="text"
+              id="name"
+              required
+              value={enteredName}
+              onChange={(event) => setEnteredName(event.target.value)}
+            />
+          </div>
+        </div>
+        <div className={classes.constrol}>
+          <label htmlFor="message">Your message</label>
+          <textarea
+            id="message"
+            rows="5"
+            required
+            value={enteredMessage}
+            onChange={(event) => setEnteredMessage(event.target.value)}
+          ></textarea>
+        </div>
+        <div className={classes.actions}>
+          <button>Send message</button>
+        </div>
+      </form>
     </section>
+  );
 }
 
 export default ContactForm;
